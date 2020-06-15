c252365b500271240e851d95c04cb4cb61025c4b
brunereduardo <33575403+brunereduardo@users.noreply.github.com>
2020-06-15 20:58:33 -0300

    Add files via upload

--- SyntacticAnalyzer.py
+++ SyntacticAnalyzer.py
@@ -5,4 +5,26 @@ class SyntacticAnalyzer() :
 
 
     print("Alanizador sintatico", '\n')
+    # primeiro usaremos o exemplo do professor
+    '''
+    def __init__(self, uma_copida_do_lexicalAnalyser?):
+        self.atr1 = uma_copida_do_lexicalAnalyser? 
+
+    def outraos procedimentos():
+
+    def begin():   
+        chamada do léxico para obtenção dos simbolos 
+        def obtrsimbl(LexicalAnalyzer):
+            x = pega o simbolo com o tolken
+        
+        chama o procedimento inicial program
+        #program(x)
+
+        if(item in lexAnalyzer.get_token_table() = fim) :
+            printf("sucesso", "\n")
+        else() :
+            print("ERRO")
+            raise ("joga pra cima o erro para ser tratado pelo modo pânico")
+
+    '''
 
